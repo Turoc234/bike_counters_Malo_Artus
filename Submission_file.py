dfffd5a5fe20af37948cf47a7327e5bb66e5ee63
Artus Mathieu <artus.mathieu5@gmail.com>
2023-12-06 18:20:32 +0100

    add submission files and modify them

--- Submission_file.py
+++ Submission_file.py
@@ -1,6 +1,52 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import Ridge
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+local_path = "data/train.parquet"
+kaggle_path = "/kaggle/input/mdsb-2023/train.parquet"
+
+
+def get_train_data(path="data/train.parquet"):
+    data = pd.read_parquet(path)
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+
+def _encode_dates(X):
+    X = X.copy()  # modify a copy of X
+    # Encode the date information from the DateOfDeparture columns
+    X["year"] = X["date"].dt.year
+    X["month"] = X["date"].dt.month
+    X["day"] = X["date"].dt.day
+    X["weekday"] = X["date"].dt.weekday
+    X["hour"] = X["date"].dt.hour
+
+    # Finally we can drop the original columns from the dataframe
+    return X.drop(columns=["date"])
+
+
+_target_column_name = "log_bike_count"
+
+if Path(local_path).exists():
+    X_train, y_train = get_train_data()
+    X_test = pd.read_parquet(local_path)
+
+elif Path(kaggle_path).exists():
+    X_train, y_train = get_train_data(path=kaggle_path)
+    X_test = pd.read_parquet(kaggle_path)
+
+else:
+    print("error in path")
+
+X_train, y_train = get_train_data()
 
 date_encoder = FunctionTransformer(_encode_dates)
 date_cols = _encode_dates(X_train[["date"]]).columns.tolist()
@@ -21,10 +67,8 @@ pipe = make_pipeline(date_encoder, preprocessor, regressor)
 pipe.fit(X_train, y_train)
 
 
-test_set = pd.read_parquet(Path("data") / "final_test.parquet")
-test_set.head()
-
-y_pred = pipe.predict(test_set)
+y_pred = pipe.predict(X_test)
+y_pred[y_pred < 0] = 0
 results = pd.DataFrame(
     dict(
         Id=np.arange(y_pred.shape[0]),
