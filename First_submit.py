3e5ca2100fcd6b5790256a1c541066f2c052920e
Artus Mathieu <artus.mathieu5@gmail.com>
2023-12-06 10:36:24 +0100

    first submit

---
+++ First_submit.py
@@ -0,0 +1,34 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
+
+date_encoder = FunctionTransformer(_encode_dates)
+date_cols = _encode_dates(X_train[["date"]]).columns.tolist()
+
+categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+categorical_cols = ["counter_name", "site_name"]
+
+preprocessor = ColumnTransformer(
+    [
+        ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
+        ("cat", categorical_encoder, categorical_cols),
+    ]
+)
+
+regressor = Ridge()
+
+pipe = make_pipeline(date_encoder, preprocessor, regressor)
+pipe.fit(X_train, y_train)
+
+
+test_set = pd.read_parquet(Path("data") / "final_test.parquet")
+test_set.head()
+
+y_pred = pipe.predict(test_set)
+results = pd.DataFrame(
+    dict(
+        Id=np.arange(y_pred.shape[0]),
+        log_bike_count=y_pred,
+    )
+)
+results.to_csv("submission.csv", index=False)
